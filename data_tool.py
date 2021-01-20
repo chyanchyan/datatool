2f540b52a579db12c901ff15dd92794a8ade9a93
chyanchyan <zhou_qianchi@163.com>
2021-01-20 20:04:06 +0800

    .

--- data_tool.py
+++ data_tool.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import os
 import sys
 
@@ -12,14 +14,6 @@ try:
 except ImportError:
     os.system('pip install numpy -i https://pypi.tuna.tsinghua.edu.cn/simple some-package')
     import numpy as np
-
-# try:
-#     import openpyxl
-# except ImportError:
-#     os.system('pip install openpyxl -i https://pypi.tuna.tsinghua.edu.cn/simple some-package')
-#     import openpyxl
-#
-# import math
 
 
 def get_file_name_info(s):
@@ -48,11 +42,13 @@ def next_file_index(s):
     else:
         return filename + '_' + str(int(index) + 1) + '.' + post_fix
 
+
 def main():
     res = []
     data_valid_flag = True
     simple_logic_mapping = {
         '求和': sum,
+        '加总': sum,
         '均值': np.mean,
         '中位数': np.median,
         '计数': len
@@ -68,14 +64,14 @@ def main():
         data_sources = pd.read_excel(config_path,
                                      sheet_name='data_sources',
                                      index_col=False,
-                                     header=0,
-                                     dtype=str)
+                                     header=0)
         logics = pd.read_excel(config_path,
                                sheet_name='logics',
                                index_col=False,
-                               header=0,
-                               dtype=str)
+                               header=0)
         pd.DataFrame.dropna(data_sources, inplace=True)
+        print(data_sources)
+
     except FileNotFoundError:
         res.append(('sys_error', '设置文件路径不存在'))
         data_valid_flag = False
@@ -91,38 +87,33 @@ def main():
 
     if data_valid_flag:
         for index, logic in logics.iterrows():
-            if not pd.isna(logic['计算逻辑']):
-                re = simple_logic_mapping[logic['计算逻辑']](
-                    data[str(logic['数据源'])][logic['数据列']]
-                )
-            else:
-                if not pd.isna(logic['advance_mode']):
-                    re = eval(logic['advance_mode'])
-                else:
-                    re = 'no logic rules applied'
-
-            if not pd.isna(logic['输出格式']):
-                re = format(re, str(logic['输出格式']))
-            else:
+            if pd.isna(logic['项目']):
                 pass
-            print(logic['项目'] + str(re))
-            res.append((logic['项目'] + ':', re))
+            else:
+                if not pd.isna(logic['计算逻辑']):
+                    re = simple_logic_mapping[logic['计算逻辑']](
+                        data[str(logic['数据源'])][logic['数据列']]
+                    )
+                else:
+                    if not pd.isna(logic['advance_mode']):
+                        re = eval(logic['advance_mode'])
+                    else:
+                        re = ''
+
+                if not pd.isna(logic['输出格式']):
+                    re = format(re, str(logic['输出格式']))
+                else:
+                    pass
+                print(str(logic['项目']) + ': ' + str(re))
+                res.append((logic['项目'], re))
 
     res = pd.DataFrame(res)
-    re_filename = '../helpers/data_tool/results.xlsx'
-    i = 0
+    re_filename = 'results.xlsx'
+
     while os.path.exists(sys.path[0] + '/' + re_filename):
         re_filename = next_file_index(re_filename)
-        print(re_filename)
+
     res.to_excel(re_filename, index=False, header=['计算项目', '计算结果'])
-    # book = openpyxl.load_workbook(config_path)
-    # writer = pd.ExcelWriter(config_path, engine=openpyxl)
-    # writer.book = book
-    # writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
-    # res.to_excel(writer, sheet_name='results', index=False)
-    # writer.save()
-
-
 
 
 if __name__ == '__main__':
